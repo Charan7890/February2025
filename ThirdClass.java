d566ecbe6c494e6a76e90c1ec0fcfaf396c59f69
Charan Naik <81156598+Charan7890@users.noreply.github.com>
2025-02-05 17:49:10 +0000

    modified ThirdClass.java and ThirdClass.class   // This is the commit message

--- ThirdClass.java
+++ ThirdClass.java
@@ -1,10 +1,16 @@
 import Folder2.InFolder1.*;
 import java.util.Scanner;
 
+import Folder1.FirstClass;
+
 class ThirdClass{
     public static void main(String[] args) {
         A a = new A();
         System.out.println(a.fact(5));
         System.out.println(a.isAnagram("listen", "silent"));
+
+        FirstClass fc = new FirstClass();
+        fc.callMe("John");
     }
 }
+
